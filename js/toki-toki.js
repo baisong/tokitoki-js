6504f95fa325049bf83ab10c45ab4a61c4251324
oren <get.oren@gmail.com>
2015-03-08 23:00:04 -0700

    add TODO

--- js/toki-toki.js
+++ js/toki-toki.js
@@ -8,6 +8,14 @@ tt.isValidWord = function (word) {
 tt.filterWords = fjs.filter(tt.isValidWord);
 
 tt.parseWords = function(words) {
+// @TODO
+// map clean/trim func
+// update valid words list
+// split into groups of "li"
+// split into sub-groups of "e"
+// validate sentence (S[VO?]*)
+// 1-to-1 gloss
+// better gloss
   return tt.filterWords(words);
 }
 
