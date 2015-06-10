e7fa2de7349e9e94169d03acb1a6a4dd7c9701dd
oren <get.oren@gmail.com>
2015-06-10 13:10:58 -0700

    Add useCanonWords() and complete isValidWord()

--- js/toki-toki.js
+++ js/toki-toki.js
@@ -1,10 +1,24 @@
 var tt = {};
 
 tt.isValidWord = function (word) {
-  var words = '|toki|pona|li|';
+  var words = '|a|akesi|ala|alasa|ale|anpa|ante|anu|awen|e|en|esun|ijo|ike|ilo|insa|jaki|jan|jelo|jo|kala|kalama|kama|kasi|ken|kepeken|kili|kiwen|ko|kon|kule|kulupu|kute|la|lape|laso|lawa|len|lete|li|lili|linja|lipu|loje|lon|luka|lukin|lupa|ma|mama|mani|meli|mi|mije|moku|moli|monsi|mu|mun|musi|mute|nanpa|nasa|nasin|nena|ni|nimi|noka|o|olin|ona|open|pakala|pali|palisa|pan|pana|pi|pilin|pimeja|pini|pipi|poka|poki|pona|pu|sama|seli|selo|seme|sewi|sijelo|sike|sin|sina|sinpin|sitelen|sona|soweli|suli|suno|supa|suwi|tan|taso|tawa|telo|tenpo|toki|tomo|tu|unpa|uta|utala|walo|wan|waso|wawa|weka|wile|';
   return words.indexOf('|' + word + '|') !== -1;
 };
 
+tt.toCanonWord = function (word) {
+  var translations = {
+    namako: 'sin',
+    ali: 'ale',
+    oko: 'lukin',
+    kin: 'a'
+  };
+  if (typeof translations.word !== 'undefined') {
+    return translations.word;
+  }
+  return word;
+}
+
+tt.useCanonWords = fjs.apply(tt.toCanonWord);
 tt.filterWords = fjs.filter(tt.isValidWord);
 
 tt.parseWords = function(words) {
