ef3019ca1154aa910cd61b4d976bf73819a16c18
oren <get.oren@gmail.com>
2015-06-10 13:24:51 -0700

    Update toki-toki.js

--- js/toki-toki.js
+++ js/toki-toki.js
@@ -1,5 +1,9 @@
 var tt = {};
 
+tt.cleanWord = function (word) {
+  return word.toLowerCase().trim();
+}
+
 tt.isValidWord = function (word) {
   var words = '|a|akesi|ala|alasa|ale|anpa|ante|anu|awen|e|en|esun|ijo|ike|ilo|insa|jaki|jan|jelo|jo|kala|kalama|kama|kasi|ken|kepeken|kili|kiwen|ko|kon|kule|kulupu|kute|la|lape|laso|lawa|len|lete|li|lili|linja|lipu|loje|lon|luka|lukin|lupa|ma|mama|mani|meli|mi|mije|moku|moli|monsi|mu|mun|musi|mute|nanpa|nasa|nasin|nena|ni|nimi|noka|o|olin|ona|open|pakala|pali|palisa|pan|pana|pi|pilin|pimeja|pini|pipi|poka|poki|pona|pu|sama|seli|selo|seme|sewi|sijelo|sike|sin|sina|sinpin|sitelen|sona|soweli|suli|suno|supa|suwi|tan|taso|tawa|telo|tenpo|toki|tomo|tu|unpa|uta|utala|walo|wan|waso|wawa|weka|wile|';
   return words.indexOf('|' + word + '|') !== -1;
@@ -18,18 +22,19 @@ tt.toCanonWord = function (word) {
   return word;
 }
 
+tt.cleanWords = fjs.map(tt.cleanWord);
 tt.useCanonWords = fjs.map(tt.toCanonWord);
 tt.filterWords = fjs.filter(tt.isValidWord);
 
 tt.parseWords = function(words) {
 // @TODO
-// map clean/trim func
-// update valid words list
+// inject hidden li for headwords: mi, sina, o.
 // split into groups of "li"
 // split into sub-groups of "e"
 // validate sentence (S[VO?]*)
 // 1-to-1 gloss
 // better gloss
+  words = tt.cleanWords(words);
   words = tt.useCanonWords(words);
   return tt.filterWords(words);
 }
